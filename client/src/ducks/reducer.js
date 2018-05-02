7ca29288db4360129ebb211b755849731b839588
Jaylan Jones <jaylanjones421@gmail.com>
2018-05-01 21:45:47 -0500

    started on ducks but need to go over what we will be handling in state

---
+++ client/src/ducks/reducer.js
@@ -0,0 +1,13 @@
+//initial state
+const initialState = {};
+//constants
+
+//action creators
+
+//reducer
+export default function reducer(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
