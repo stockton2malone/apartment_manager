f1b5af093821a4472069c07c045609b48db68f23
Michael J Cooper <ballermjc@gmail.com>
2018-05-02 19:35:14 -0600

    split the controller and updated the server file accordingly

--- server/controllers/notesController.js
+++ server/controllers/notesController.js
@@ -31,41 +31,6 @@ module.exports = {
 
         imageManager.getVideo(id, transformations, res);
     },
-    getTicket: (req, res, next) => {
-        let dbInstance = req.app.get('db');
-
-        // read from DB
-    },
-    createTicket: (req, res, next) => {
-        let dbInstance = req.app.get('db');
-
-        // read from DB
-    },
-    updateTicket: (req, res, next) => {
-        let dbInstance = req.app.get('db');
-
-        // read from DB
-    },
-    deleteTicket: (req, res, next) => {
-        let dbInstance = req.app.get('db');
-
-        // read from DB
-    },
-    getOwnerTickets: (req, res, next) => {
-        let dbInstance = req.app.get('db');
-
-        // read from DB
-    },
-    getMaintenanceTickets: (req, res, next) => {
-        let dbInstance = req.app.get('db');
-
-        // read from DB
-    },
-    getTenantTickets: (req, res, next) => {
-        let dbInstance = req.app.get('db');
-
-        // read from DB
-    },
     getNotes: (req, res, next) => {
         let dbInstance = req.app.get('db');
 
---
+++ server/controllers/ticketsController.js
@@ -0,0 +1,37 @@
+module.exports = {
+    getTicket: (req, res, next) => {
+        let dbInstance = req.app.get('db');
+
+        // read from DB
+    },
+    createTicket: (req, res, next) => {
+        let dbInstance = req.app.get('db');
+
+        // read from DB
+    },
+    updateTicket: (req, res, next) => {
+        let dbInstance = req.app.get('db');
+
+        // read from DB
+    },
+    deleteTicket: (req, res, next) => {
+        let dbInstance = req.app.get('db');
+
+        // read from DB
+    },
+    getOwnerTickets: (req, res, next) => {
+        let dbInstance = req.app.get('db');
+
+        // read from DB
+    },
+    getMaintenanceTickets: (req, res, next) => {
+        let dbInstance = req.app.get('db');
+
+        // read from DB
+    },
+    getTenantTickets: (req, res, next) => {
+        let dbInstance = req.app.get('db');
+
+        // read from DB
+    }
+}
