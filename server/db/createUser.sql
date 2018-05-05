60f7acfcb10f5109a21f85b9a7d308a2405db4f9
brettghays <brett.g.hays.coding@gmail.com>
2018-05-05 11:04:19 -0600

    auth endpoints working

--- server/db/createUser.sql
+++ server/db/createUser.sql
@@ -1,10 +1,7 @@
-INSERT INTO Tickets (
+INSERT INTO Users (
+    user_id,
     user_name,
-    user_address,
     user_unit,
-    user_city,
-    user_state,
-    user_zip,
     user_complex,
     user_email,
     user_phone,
@@ -12,4 +9,4 @@ INSERT INTO Tickets (
     active_status,
     user_role
 )
-VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12);
+VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9);
