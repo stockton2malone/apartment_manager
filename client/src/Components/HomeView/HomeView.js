f0347dff34143b0518cb9cac1e381f207b255d57
Jaylan Jones <jaylanjones421@gmail.com>
2018-05-05 13:11:10 -0500

    made tickets able to dynamically render and added new ticket button

--- client/src/Components/HomeView/HomeView.js
+++ client/src/Components/HomeView/HomeView.js
@@ -1,21 +1,40 @@
 import React, { Component } from "react";
 import TicketRow from "../TicketRow/TicketRow";
 import "./HomeView.css";
+import { Link } from "react-router-dom";
 
 class HomeView extends Component {
   render() {
+    let tickets = this.props.tickets ? (
+      this.props.tickets.map((ticket, i) => (
+        <div key={i}>
+          <TicketRow
+            ticketID={ticket.ticket_id}
+            complexName={ticket.complex_id}
+            unitNumber={ticket.unitNumber}
+            ticketTitle={ticket.issue_description}
+            ticketTime={ticket.creation_date}
+          />
+        </div>
+      ))
+    ) : (
+      <div>
+        <h2>NO ACTIVE TICKETS</h2>
+      </div>
+    );
+
     return (
       <div className="HomeViewContainer">
         <div className="HomeViewTitle">
           <h1>Tickets</h1>
         </div>
         <div className="ticketContainer">
+          {tickets}
+          dummy row v
           <TicketRow />
-          <TicketRow />
-          <TicketRow />
-          <TicketRow />
-          <TicketRow />
-          <TicketRow />
+          <Link to="/wizard1">
+            <div className="newTicketButton">New Ticket</div>
+          </Link>
         </div>
       </div>
     );
