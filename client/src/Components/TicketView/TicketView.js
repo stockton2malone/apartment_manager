cafa46adbd914877b13a235c9e3d633a1e665a8e
Jordan <insomniacrampage@hotmail.com>
2018-05-09 18:26:51 -0600

    ticket view

--- client/src/Components/TicketView/TicketView.js
+++ client/src/Components/TicketView/TicketView.js
@@ -11,47 +11,65 @@ class TicketView extends Component {
           ? this.props.assignee
           : " Unassigned"}`}</h2>
         <div className="ticketInfoContainer">
-          <div className="dateAssigned">
-            <h3>Assigned On:</h3>
+          <div className="dateAssigned inlay">
+            <span className='lrg'>Assigned On:</span>
             {`${this.props.assignedDate
               ? this.props.assignedDate
-              : " Unassigned"}`}
+              : new Date().toLocaleString()}`}
+          </div>
+          <div className="dateCompleted inlay">
+            <span className='lrg'>Completed On:</span>
+            {`${this.props.completedDate
+              ? this.props.completedDate
+              : "N/A"}`}
           </div>
 
-          <div className="ticketDescription">
-            <h3>Description:</h3>
-            {`${this.props.description ? this.props.description : "lorem"}`}
+          <div className="ticketDescription inlay">
+            <span className='lrg'>Description:</span>
+            {`${this.props.description ? this.props.description : "lorem ipsum yall"}`}
           </div>
-          <div className="urgencyType">
+          <div className="inlay">
             <div>
-              <h3>Urgency:</h3>
-              {`${this.props.urgency ? this.props.urgency : "Critical"}`}
+              <span className='lrg'>Urgency:</span>
+              <span className={this.props.urgency ? this.props.urgency : "critical"}>{`${this.props.urgency ? this.props.urgency : "Critical"}`}</span>
             </div>
-            <div>
-              <h3>Type:</h3>
-              {`${this.props.type ? this.props.type : "New"}`}
+            
+          </div>
+          <div className="inlay"><div>
+              <span className='lrg'>Type:</span>
+              {`${this.props.type ? this.props.type : "New"}`}</div>
             </div>
+          <div className="noteContainer inlay">
+            <div className='existing-note'>
+              <div className="attachment"><i className="fas fa-paperclip"></i></div>
+              <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed nisl neque. Nulla nec tortor eget dolor vulputate tincidunt. Vestibulum quis dignissim nulla. Integer ullamcorper porta lectus. Suspendisse fermentum at nisi id semper. Etiam dictum varius aliquet. Curabitur maximus porttitor maximus. Nulla vehicula vitae tortor ac pretium. Cras mi mi, malesuada eu urna eu, pharetra efficitur dolor.
+              </div>
+            </div>
+            <div className='existing-note'>
+              <div className="attachment"><i className="fas fa-paperclip"></i></div>
+              <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. In sed nisl neque. Nulla nec tortor eget dolor vulputate tincidunt. Vestibulum quis dignissim nulla. Integer ullamcorper porta lectus. Suspendisse fermentum at nisi id semper. Etiam dictum varius aliquet. Curabitur maximus porttitor maximus. Nulla vehicula vitae tortor ac pretium. Cras mi mi, malesuada eu urna eu, pharetra efficitur dolor.
+              </div>
+            </div>
+            <div className="inlay" onClick={() => alert("get the ducks going")}>+ Add Note</div>
           </div>
-          <div className="noteContainer">
-            <div>first note</div>
-            <div onClick={() => alert("get the ducks going")}>+ Add Note</div>
-          </div>
-          <h3>Ticket Status</h3>
+          <div className="inlay">
+          <span className="lrg">Ticket Status</span>
           {this.props.userRole === "tenant" ? (
-            <div className="ticketStatus">
+            <div className="ticketStatus inlay">
               {this.props.ticketStatus
                 ? this.props.ticketStatus
                 : "Placeholder"}
             </div>
           ) : (
-            <select name="ticketStatus" id="statusSelect">
-              <option value="New">New</option>
-              <option value="Assigned">Assigned</option>
-              <option value="In Process">In Process</option>
-              <option value="Canceled">Canceled</option>
-              <option value="Completed">Completed</option>
-            </select>
-          )}
+              <select name="ticketStatus" id="statusSelect">
+                <option value="New">New</option>
+                <option value="Assigned">Assigned</option>
+                <option value="In Process">In Process</option>
+                <option value="Canceled">Canceled</option>
+                <option value="Completed">Completed</option>
+              </select>
+            )}
+            </div>
         </div>
       </div>
     );
