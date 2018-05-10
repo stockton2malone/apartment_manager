5d7505dfeafb409bc9b00c31a333ea91c285eb63
Jaylan Jones <jaylanjones421@gmail.com>
2018-05-09 20:55:10 -0500

    whoops, had the assets outside of src

--- client/src/Components/HomeView/HomeView.js
+++ client/src/Components/HomeView/HomeView.js
@@ -6,6 +6,9 @@ import { withRouter } from "react-router-dom";
 
 import TicketRow from "../TicketRow/TicketRow";
 import "./HomeView.css";
+import logo1 from "../../../assets/Rent (2).png";
+import logo2 from "../../../assets/Rent (3).png";
+
 import {
   setUserID,
   setUserName,
