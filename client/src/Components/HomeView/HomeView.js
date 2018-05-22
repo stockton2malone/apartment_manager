0ca332a16a9c5547dd13c4b24728da54cc454223
Jaylan Jones <jaylanjones421@gmail.com>
2018-05-21 20:19:05 -0500

    made it so that the colors on the ticket rows actually change based on status

--- client/src/Components/HomeView/HomeView.js
+++ client/src/Components/HomeView/HomeView.js
@@ -12,18 +12,29 @@ import { setTickets } from "../../ducks/reducer";
 
 class HomeView extends Component {
   componentDidMount() {
+    //console.log("userID: ", this.props.userID)
+    //console.log('userRole: ', this.props.userRole)
     if (this.props.userRole === "Owner") {
-      axios.get(`/api/tickets/owner/${this.props.userID}`).then(res => {
-        this.setTickets(res.data);
-      });
+      axios
+        .get(`/api/tickets/owner/${this.props.userID}`)
+        .then(res => {
+          this.props.setTickets(res.data);
+        })
+        .catch(err => console.log(err));
     } else if (this.props.userRole === "Worker") {
-      axios.get(`/api/tickets/maintenance/${this.props.userID}`).then(res => {
-        this.setTickets(res.data);
-      });
+      axios
+        .get(`/api/tickets/worker/${this.props.userID}`)
+        .then(res => {
+          this.props.setTickets(res.data);
+        })
+        .catch(err => console.log(err));
     } else if (this.props.userRole === "Tenant") {
-      axios.get(`/api/tickets/tenant/${this.props.userID}`).then(res => {
-        this.setTickets(res.data);
-      });
+      axios
+        .get(`/api/tickets/tenant/${this.props.userID}`)
+        .then(res => {
+          this.props.setTickets(res.data);
+        })
+        .catch(err => console.log(err));
     }
   }
 
@@ -31,29 +42,33 @@ class HomeView extends Component {
     let tickets =
       this.props.tickets.length > 0 ? (
         this.props.tickets.map((ticket, i) => (
-          <div key={i}>
-            <TicketRow
-              ticketID={ticket.ticket_id}
-              complexName={ticket.complex_id}
-              unitNumber={ticket.unitNumber}
-              ticketTitle={ticket.issue_description}
-              ticketTime={ticket.creation_date}
-            />
-          </div>
+          <TicketRow
+            key={i}
+            ticketID={ticket.ticket_id}
+            complexName={ticket.complex_id}
+            unitNumber={ticket.unitNumber}
+            ticketTitle={ticket.issue_description}
+            ticketTime={ticket.creation_date}
+            status={ticket.ticket_status}
+          />
         ))
       ) : (
         <div>
           <h2>NO ACTIVE TICKETS</h2>
         </div>
       );
-
+    console.log(this.props.tickets);
     return (
       <div className="HomeViewContainer">
         <div className="HomeViewTitle">
           <img className="homeViewImg" src={logo1} alt="" srcset="" />
         </div>
         <div className="infoContainer">
-          <div className="ticketContainer">{tickets}</div>
+          <div className="ticketContainer">
+            <table className="ticketTable">
+              <tbody className="ticketTableBody">{tickets}</tbody>
+            </table>
+          </div>
         </div>
         <div className="buttonDiv">
           <Link to="/wizard1">
--- client/src/Components/TicketRow/TicketRow.js
+++ client/src/Components/TicketRow/TicketRow.js
@@ -6,23 +6,27 @@ class TicketRow extends Component {
   render() {
     let styles = status => {
       let styled;
-      if (this.props.status === "urgent") {
+      if (this.props.status === "New") {
         styled = {
+          //pink
           backgroundColor: "#F99AB5"
         };
       }
-      if (this.props.status === "cancelled") {
+      if (this.props.status === "Canceled") {
         styled = {
+          //orange
           backgroundColor: "#F9C49A"
         };
       }
-      if (this.props.status === "new") {
+      if (this.props.status === "Assigned") {
         styled = {
+          //yellow
           backgroundColor: "#F9F69A"
         };
       }
-      if (this.props.status === "assigned") {
+      if (this.props.status === "In Process") {
         styled = {
+          //teal
           backgroundColor: "#9AF9F1"
         };
       }
@@ -31,41 +35,47 @@ class TicketRow extends Component {
     return (
       <Link to={`/ticket/${this.props.ticketID}`}>
         {this.props.userRole === "Tenant" ? (
-          <div className="ticketRowContainer">
-            <div className="ticketAssignee">
+          <tr className="ticketRowContainer">
+            <td className="ticketAssignee">
               {this.props.worker_id ? this.props.worker_id : "Not Yet Assigned"}
-            </div>
-            <div className="ticketStatus">
+            </td>
+            <td className="ticketStatus">
               {this.props.status ? this.props.status : "Completed"}
-            </div>
-            <div className="ticketTitle">
+            </td>
+            <td className="ticketTitle">
               {this.props.ticketTitle
                 ? this.props.ticketTitle
-                : "Broken Washer"}
-            </div>
-            <div id="ticketTime" style={styles(this.props.status)}>
-              {this.props.ticketTime ? this.props.ticketTime : "MON: 3pm"}
-            </div>
-          </div>
+                : " askjdhf jklahsdf ljkahsd asdkjf halksdfh ljkashf  asd fjhaskjldf h"}
+            </td>
+            <td id="ticketTime" style={styles(this.props.status)}>
+              {this.props.ticketTime
+                ? this.props.ticketTime.slice(5, 10)
+                : "MON: 3pm"}
+            </td>
+          </tr>
         ) : (
-          <div className="ticketRowContainer">
-            <div className="complexName">
+          <tr className="ticketRowContainer">
+            <td className="complexName">
               {this.props.complexName
                 ? this.props.complexName
-                : "Butler Brother's"}
-            </div>
-            <div className="unitNumber">
-              {this.props.unitNumber ? this.props.unitNumber : "222"}
-            </div>
-            <div className="ticketTitle">
+                : "Butler Brother's more more more more"}
+            </td>
+            <td className="unitNumber">
+              {this.props.unitNumber
+                ? this.props.unitNumber
+                : "222 more more more more"}
+            </td>
+            <td className="ticketTitle">
               {this.props.ticketTitle
                 ? this.props.ticketTitle
-                : "Broken Washer"}
-            </div>
-            <div id="ticketTime" style={styles(this.props.status)}>
-              {this.props.ticketTime ? this.props.ticketTime : "MON: 3pm"}
-            </div>
-          </div>
+                : " sdfg jsk sdlfg j;klsdj ojsf;glk jsdl gj Washer"}
+            </td>
+            <td id="ticketTime" style={styles(this.props.status)}>
+              {this.props.ticketTime
+                ? this.props.ticketTime.slice(5, 10)
+                : "MON: 3pm more more more more "}
+            </td>
+          </tr>
         )}
       </Link>
     );
